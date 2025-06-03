4d79b702a2c4238deae07749666f9644c764401e
karin <ovbkarin@gmail.com>
2025-06-03 00:39:41 -0400

    3rd trial to deploy infra

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            =  "prod/terraform.tfstate"
+    key            =  "dev/terraform.state"
     #key            = "dev/terraform.state"
     bucket         = "infra-tech-bucket"
     #bucket         = "class38dominion-terraform-backend"
--- prod/backend.tf
+++ prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            = "prod/terraform.tfstate"
+    key            = "prod/terraform.state"
     #key            = "terraform/state"
     bucket         = "infra-tech-bucket"
     region         = "us-east-1"
